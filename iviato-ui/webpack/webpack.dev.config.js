8e69811fcdb69c7d3ff9780f9e5f40d351cbc613
Hirad Pourtahmasbi <hirad.pourtahmasbi@gmail.com>
2018-05-12 09:24:02 -0700

    Initial dockerization for ui

--- iviato-ui/webpack/webpack.dev.config.js
+++ iviato-ui/webpack/webpack.dev.config.js
@@ -4,26 +4,32 @@ var path = require('path');
 var parentDir = path.join(__dirname, '../');
 
 module.exports = {
-    entry: [
-        path.join(parentDir, 'index.js')
-    ],
-    module: {
-        loaders: [{
-            test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader'
-            },{
-                test: /\.less$/,
-                loaders: ["style-loader", "css-loder", "less-loader"]
-            }
-        ]
-    },
-    output: {
-        path: parentDir + '/dist',
-        filename: 'bundle.js'
-    },
-    devServer: {
-        contentBase: parentDir,
-        historyApiFallback: true
-    }
+	entry: [
+		path.join(parentDir, 'index.js')
+	],
+	module: {
+		loaders: [{
+			test: /\.(js|jsx)$/,
+			exclude: /node_modules/,
+			loader: 'babel-loader'
+		}, {
+			test: /\.(less)$/,
+			loaders: ["style-loader", "css-loder", "less-loader"]
+		}, {
+			test: /\.scss$/,
+			use: [
+					"style-loader", // creates style nodes from JS strings
+					"css-loader", // translates CSS into CommonJS
+					"sass-loader" // compiles Sass to CSS
+			]
+		}]
+	},
+	output: {
+		path: parentDir + '/dist',
+		filename: 'bundle.js'
+	},
+	devServer: {
+		contentBase: parentDir,
+		historyApiFallback: true
+	}
 }
